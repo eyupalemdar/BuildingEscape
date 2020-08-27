0205fea1628967602c150799e43e9f02941929e7
eyupalemdar <eyupalemdar@outlook.com>
2020-08-28 01:04:31 +0300

    Rotating an Actor with Code is implemented

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,45 @@
+// Copyrights AlemdarLabs 2020
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	FRotator CurrentRotation = GetOwner()->GetActorRotation();
+
+	UE_LOG(LogTemp, Warning, TEXT("Current Yaw: %f"), CurrentRotation.Yaw);
+
+	CurrentRotation.Yaw += 90;
+
+	UE_LOG(LogTemp, Warning, TEXT("After Rotation Yaw: %f"), CurrentRotation.Yaw);
+
+	bool IsSuccessfullyRotated = GetOwner()->SetActorRotation(CurrentRotation);
+
+	UE_LOG(LogTemp, Warning, TEXT("Is Successfully Rotated: %b"), IsSuccessfullyRotated);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
